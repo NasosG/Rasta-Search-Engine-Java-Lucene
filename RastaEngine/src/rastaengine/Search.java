a222172a11d2fa71a020ef69a6f3afeaf81493c8
NasosG <53830262+NasosG@users.noreply.github.com>
2020-06-11 04:58:17 +0300

    2nd commit

--- RastaEngine/src/rastaengine/Search.java
+++ RastaEngine/src/rastaengine/Search.java
@@ -1,7 +1,4 @@
-/*
- * Galatis Athanasios AM: 2022201500017 email: dit15017@uop.gr
- * Vakouftsis Athanasios AM: 2022201500007 email: dit15007@uop.gr
- */
+
 package rastaengine;
 
 import java.io.IOException;
